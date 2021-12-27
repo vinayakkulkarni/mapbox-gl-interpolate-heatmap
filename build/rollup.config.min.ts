40d4bd625f0cb80d8e3ec64c75b424b9185e9b7b
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2021-12-27 15:44:37 +0530

    build: with rollup config

---
+++ build/rollup.config.min.ts
@@ -0,0 +1,62 @@
+import alias from '@rollup/plugin-alias';
+import babel from '@rollup/plugin-babel';
+import beep from '@rollup/plugin-beep';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import sucrase from '@rollup/plugin-sucrase';
+import { terser } from 'rollup-plugin-terser';
+import pkg from '../package.json';
+
+const plugins = [
+  alias(),
+  resolve({
+    extensions: ['.js', '.ts'],
+    browser: true,
+  }),
+  babel({
+    babelHelpers: 'bundled',
+    exclude: 'node_modules/**',
+  }),
+  commonjs({
+    extensions: ['.js', '.ts'],
+    exclude: 'src/**',
+    include: 'node_modules/**',
+  }),
+  sucrase({
+    exclude: ['node_modules/**'],
+    transforms: ['typescript'],
+  }),
+  beep(),
+  terser({
+    compress: {
+      drop_console: true,
+      drop_debugger: true,
+    },
+  }),
+];
+
+const banner = `/*!
+  * ${pkg.name} v${pkg.version}
+  * (c) ${new Date().getFullYear()} ${pkg.author.name}
+  * @license ${pkg.license}
+  */`;
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      format: 'umd',
+      file: pkg.cdn,
+      name: pkg.name,
+      exports: 'named',
+      banner,
+      sourcemap: true,
+      globals: {
+        earcut: 'earcut',
+        'mapbox-gl': 'mapboxgl',
+      },
+    },
+  ],
+  plugins,
+  external: ['mapbox-gl', 'earcut'],
+};
---
+++ build/rollup.config.ts
@@ -0,0 +1,72 @@
+import alias from '@rollup/plugin-alias';
+import babel from '@rollup/plugin-babel';
+import beep from '@rollup/plugin-beep';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import sucrase from '@rollup/plugin-sucrase';
+import pkg from '../package.json';
+
+const extensions = ['.js', '.ts'];
+
+const plugins = [
+  alias(),
+  resolve({ extensions, browser: true }),
+  babel({
+    babelHelpers: 'bundled',
+    exclude: 'node_modules/**',
+  }),
+  commonjs({ extensions, exclude: 'src/**', include: 'node_modules' }),
+  sucrase({
+    exclude: ['node_modules/**'],
+    transforms: ['typescript'],
+  }),
+  beep(),
+];
+
+const banner = `/*!
+ * ${pkg.name} v${pkg.version}
+ * ${pkg.description}
+ * (c) 2021 ${pkg.author.name}<${pkg.author.email}>
+ * Released under the ${pkg.license} License
+ */
+`;
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    // ESM build to be used with webpack/rollup.
+    {
+      format: 'es',
+      file: pkg.module,
+      name: pkg.name,
+      exports: 'named',
+      sourcemap: true,
+      banner,
+    },
+    // CommonJS build
+    {
+      format: 'cjs',
+      file: pkg.main,
+      name: pkg.name,
+      exports: 'named',
+      strict: true,
+      sourcemap: true,
+      banner,
+    },
+    // UMD build.
+    {
+      format: 'umd',
+      file: pkg.umd,
+      name: pkg.name,
+      exports: 'named',
+      sourcemap: true,
+      banner,
+      globals: {
+        earcut: 'earcut',
+        'mapbox-gl': 'mapboxgl',
+      },
+    },
+  ],
+  plugins,
+  external: ['mapbox-gl', 'earcut'],
+};
